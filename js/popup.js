c6ce1f7c54eba30a5b17dbc1d98c18bd951cbf9a
Mark Lintern <markllintern@gmail.com>
2019-05-21 13:31:07 -0400

    task and style updates

--- js/popup.js
+++ js/popup.js
@@ -27,58 +27,79 @@ chrome.tabs.query({'active': true, 'lastFocusedWindow': true}, function (tabs) {
     $('.auction-bid').val(vars['auctionBid']);
   }
   var now = new Date();
-  var day = ("0" + now.getDate()).slice(-2);
-  var month = ("0" + (now.getMonth() + 1)).slice(-2);
-  var today = now.getFullYear()+"-"+(month)+"-"+(day) ;
+  var day = ('0' + now.getDate()).slice(-2);
+  var month = ('0' + (now.getMonth() + 1)).slice(-2);
+  var today = now.getFullYear() + "-" + month + "-" + day ;
+  var hour = ('0' + (now.getHours() + 1)).slice(-2);
+  var min = ('0' + (now.getMinutes() + 1)).slice(-2);
 
   $('.auction-date').val(today);
-  $('.auction-time').val(now.getHours() + ':' + now.getMinutes());
+  $('.auction-time').val(hour + ':' + min);
 });
 
 function createTask(id, bid, timestamp) {
-  var now = '2019-5-20T12:00:00-08:00';
+  var now = new Date();
+  var year = now.getFullYear();
+  var day = ('0' + now.getDate()).slice(-2);
+  var month = ('0' + (now.getMonth() + 1)).slice(-2);
+  var hour = ('0' + (now.getHours() + 1)).slice(-2);
+  var min = ('0' + (now.getMinutes() + 1)).slice(-2);
+  var sec = ('0' + (now.getSeconds() + 1)).slice(-2);
+
+  var currentTime = year + "-" + month + "-" + day + 'T' + hour + ':' + min + ':' + sec + '.' + now.getMilliseconds();
+  // '2019-5-20T12:00:00-08:00';
+
   var command = 'ruby file.rb ' + id + ' ' + bid + ' x y';
   console.log(timestamp);
   return [
-    '<?xml version="1.0" ?>',
-    '<Task xmlns="https://schemas.microsoft.com/windows/2004/02/mit/task">',
+    '<?xml version="1.0" encoding="UTF-16"?>',
+    '<Task version="1.2" xmlns="http://schemas.microsoft.com/windows/2004/02/mit/task">',
     '  <RegistrationInfo>',
-    '    <Date>',
-          now,
-    '    </Date>',
-    '    <Author>Nretnil</Author>',
-    '    <Version>1.0.0</Version>',
-    '    <Description>Make a Bid at a Specific Time</Description>',
+    '    <Date>2019-05-09T13:54:15.1926486</Date>',
+    '    <Author>DESKTOP-GPH8FUF\\FBA laptop</Author>',
+    '    <Description>' + id + ' Bid on Auction</Description>',
+    '    <URI>\\' + id + ' Bid on Auction</URI>',
     '  </RegistrationInfo>',
     '  <Triggers>',
     '    <TimeTrigger>',
-    '      <StartBoundary>',
-            timestamp,
-    '      </StartBoundary>',
-    '      <EndBoundary>2006-01-01T00:00:00-08:00</EndBoundary>',
+    '      <StartBoundary>' + timestamp + '</StartBoundary>',
     '      <Enabled>true</Enabled>',
-    '        <ExecutionTimeLimit>PT5M</ExecutionTimeLimit>',
-    '     </TimeTrigger>',
+    '    </TimeTrigger>',
     '  </Triggers>',
     '  <Principals>',
-    '    <Principal>',
-    '      <UserId>Administrator</UserId>',
+    '    <Principal id="Author">',
+    '      <UserId>S-1-5-21-1770539047-2666447036-182095672-1001</UserId>',
     '      <LogonType>InteractiveToken</LogonType>',
+    '      <RunLevel>LeastPrivilege</RunLevel>',
     '    </Principal>',
     '  </Principals>',
     '  <Settings>',
-    '    <Enabled>true</Enabled>',
-    '    <AllowStartOnDemand>true</AllowStartOnDemand>',
+    '    <MultipleInstancesPolicy>IgnoreNew</MultipleInstancesPolicy>',
+    '    <DisallowStartIfOnBatteries>false</DisallowStartIfOnBatteries>',
+    '    <StopIfGoingOnBatteries>true</StopIfGoingOnBatteries>',
     '    <AllowHardTerminate>true</AllowHardTerminate>',
+    '    <StartWhenAvailable>false</StartWhenAvailable>',
+    '    <RunOnlyIfNetworkAvailable>true</RunOnlyIfNetworkAvailable>',
+    '    <IdleSettings>',
+    '      <StopOnIdleEnd>true</StopOnIdleEnd>',
+    '      <RestartOnIdle>false</RestartOnIdle>',
+    '    </IdleSettings>',
+    '    <AllowStartOnDemand>true</AllowStartOnDemand>',
+    '    <Enabled>true</Enabled>',
+    '    <Hidden>false</Hidden>',
+    '    <RunOnlyIfIdle>false</RunOnlyIfIdle>',
+    '    <WakeToRun>false</WakeToRun>',
+    '    <ExecutionTimeLimit>PT72H</ExecutionTimeLimit>',
+    '    <Priority>7</Priority>',
     '  </Settings>',
-    '  <Actions>',
-    '   <Exec>',
-    '      <Command>',
-            command,
-    '      </Command>',
+    '  <Actions Context="Author">',
+    '    <Exec>',
+    '      <Command>C:\\Ruby26-x64\\bin\\ruby.exe</Command>',
+    '      <Arguments>auction_bidder.rb ' + id + ' ' + bid + ' x y</Arguments>',
+    '      <WorkingDirectory>C:\\mark\\auction_bidder</WorkingDirectory>',
     '    </Exec>',
     '  </Actions>',
-    '</Task>',
+    '</Task>'
   ].join('\n');
 }
 
@@ -87,7 +108,7 @@ $('.auction-to-task-form').on('submit', function (event) {
 
   var id = $('.auction-id').val();
   var bid = $('.auction-bid').val();
-  var timestamp = $('.auction-date').val() + 'T' + $('.auction-time').val() + ':00-500';
+  var timestamp = $('.auction-date').val() + 'T' + $('.auction-time').val() + ':00';
 
   var doc = createTask(id, bid, timestamp);
 
@@ -96,6 +117,6 @@ $('.auction-to-task-form').on('submit', function (event) {
 
   chrome.downloads.download({
     url: url,
-    filename: 'task_' + id + '.xml'
+    filename: id + '_bid_task' + '.xml'
   });
 });
